a3f46f5657fb06640f0e1ddc1777351364643570
Boris Filipov <boris.filipov@soundcloud.com>
2019-05-31 11:45:20 +0200

    Make #curry generic again

--- src/main/scala/Chapter2Curry.scala
+++ src/main/scala/Chapter2Curry.scala
@@ -1,10 +1,10 @@
 object Chapter2Curry {
 
-  def curry(f: (Int, String) => Long): Int => (String => Long) =
-    (a: Int) => (b: String) => f(a, b)
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
 
   def uncurry[A, B, C](f: A => (B => C)): (A, B) => C =
-    (a: A, b: B) => f(a)
+    (a: A, b: B) => f(a)(b)
 
   def compose[A, B, C](f: B => C, g: A => B): A => C = ???
 
